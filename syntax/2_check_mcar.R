c818edb7109a2754eb729abf16423fc06d89e492
Jeremy Eberle <jwe4ec@virginia.edu>
2021-05-01 22:06:52 -0400

    Add 2_check_mcar.R

---
+++ syntax/2_check_mcar.R
@@ -0,0 +1,65 @@
+# ---------------------------------------------------------------------------- #
+# Check MCAR
+# Author: Jeremy W. Eberle
+# ---------------------------------------------------------------------------- #
+
+# ---------------------------------------------------------------------------- #
+# Set working directory ----
+# ---------------------------------------------------------------------------- #
+
+# Use setwd function to set working directory to the folder containing files
+# downloaded from the Open Science Framework (https://osf.io/yn3z6/).
+
+# ---------------------------------------------------------------------------- #
+# Notes ----
+# ---------------------------------------------------------------------------- #
+
+# TODO
+
+
+
+
+
+# ---------------------------------------------------------------------------- #
+# Store working directory, install correct R version, load packages ----
+# ---------------------------------------------------------------------------- #
+
+wd_dir <- getwd()
+
+script_R_version <- "R version 4.0.3 (2020-10-10)"
+current_R_version <- R.Version()$version.string
+
+if(current_R_version != script_R_version) {
+  warning(paste0("This script is based on ", script_R_version,
+                 ". You are running ", current_R_version, "."))
+}
+
+library(groundhog)
+groundhog_day <- "2021-04-01"
+
+# ---------------------------------------------------------------------------- #
+# Import further cleaned CSV data files ----
+# ---------------------------------------------------------------------------- #
+
+# Obtain file names of CSV data files
+
+further_clean_data_dir <- paste0(wd_dir, "/data/clean_further")
+filenames <- list.files(paste0(further_clean_data_dir),
+                        pattern = "*.csv",
+                        full.names = FALSE)
+
+# Import data files and store them in list
+
+# TODO
+
+
+
+
+
+# Name each data file in list
+
+# TODO
+
+
+
+
