04d23e1f4014e40458a6aca85887362eb12ad660
Jeremy Eberle <jwe4ec@virginia.edu>
2021-05-10 00:59:37 -0400

    Add analysis variables to analysis tables

--- syntax/6_run_analyses.R
+++ syntax/6_run_analyses.R
@@ -40,9 +40,6 @@ groundhog_day <- version_control()
 
 groundhog.library(nlme, groundhog_day)
 
-
-
-
 # ---------------------------------------------------------------------------- #
 # Import data ----
 # ---------------------------------------------------------------------------- #
@@ -55,51 +52,120 @@ load("./data/intermediate/data3.Rdata")
 
 # TODO: Add other key analysis variables in each table below
 
+# Identify key analysis variables
 
-
-
+meanDSS_vars <- c("meanDSS", "meanDSS_btw", "meanDSS_wth",
+                  "meanDSS_pomp", "meanDSS_pomp_btw", "meanDSS_pomp_wth")
+drtotl_m_imp_vars <- c("drtotl_m_imp", "drtotl_m_imp_btw", "drtotl_m_imp_wth",
+                       "drtotl_m_imp_pomp", "drtotl_m_imp_pomp_btw", 
+                         "drtotl_m_imp_pomp_wth")
+cnDoSS_vars <- c("cnDoSS", "cnDoSS_btw", "cnDoSS_wth",
+                 "cnDoSS_pomp", "cnDoSS_pomp_btw", "cnDoSS_pomp_wth")
+KMTOT_vars <- c("KMTOT", "KMTOT_btw", "KMTOT_wth",
+                "KMTOT_pomp", "KMTOT_pomp_btw", "KMTOT_pomp_wth")
 
 # Create table for contemporaneous model
 
-contemp <- data3$dbt_wccl[, c("ResearchID", "Condition", "AIN", "Period",
-                              "time0", "meanDSS")]
-contemp <- merge(contemp,
-                 data3$ders[, c("ResearchID", "time0", "drtotl")],
+contemp <- data3$dbt_wccl[, c("ResearchID", "Condition", "AIN", "Period", 
+                              "time0", meanDSS_vars)]
+contemp <- merge(contemp, 
+                 data3$ders[, c("ResearchID", "time0", drtotl_m_imp_vars)],
+                 by = c("ResearchID", "time0"),
+                 all.x = TRUE)
+contemp <- merge(contemp, 
+                 data3$doss[, c("ResearchID", "time0", cnDoSS_vars)],
                  by = c("ResearchID", "time0"),
                  all.x = TRUE)
 contemp <- merge(contemp,
-                 data3$doss[, c("ResearchID", "time0", "cnDoSS")],
-                 by = c("ResearchID", "time0"),
-                 all.x = TRUE)
-contemp <- merge(contemp,
-                 data3$kims[, c("ResearchID", "time0", "KMTOT")],
+                 data3$kims[, c("ResearchID", "time0", KMTOT_vars)],
                  by = c("ResearchID", "time0"),
                  all.x = TRUE)
 
 # Create table for lagged model
 
 lagged <- data3$dbt_wccl[, c("ResearchID", "Condition", "AIN", "Period",
-                             "time0_lag", "meanDSS")]
+                             "time0_lag", meanDSS_vars)]
 lagged <- merge(lagged,
-                data3$ders[, c("ResearchID", "time0_lag", "drtotl")],
+                data3$ders[, c("ResearchID", "time0_lag", drtotl_m_imp_vars)],
                 by = c("ResearchID", "time0_lag"),
                 all.x = TRUE)
 lagged <- merge(lagged,
-                data3$doss[, c("ResearchID", "time0_lag", "cnDoSS")],
+                data3$doss[, c("ResearchID", "time0_lag", cnDoSS_vars)],
                 by = c("ResearchID", "time0_lag"),
                 all.x = TRUE)
 lagged <- merge(lagged,
-                data3$kims[, c("ResearchID", "time0_lag", "KMTOT")],
+                data3$kims[, c("ResearchID", "time0_lag", KMTOT_vars)],
                 by = c("ResearchID", "time0_lag"),
                 all.x = TRUE)
 lagged <- lagged[!is.na(lagged$time0_lag), ]
 
 # ---------------------------------------------------------------------------- #
-# Run analyses ----
+# Run analyses for intent-to-treat sample ----
 # ---------------------------------------------------------------------------- #
+
+# Contemporaneous models using POMP scores
 
 # TODO
 
+
+
+
+
+# Contemporaneous models using average item scores
+
+# TODO
+
+
+
+
+
+# Lagged models using POMP scores
+
+# TODO
+
+
+
+
+
+# Lagged models using average item scores
+
+# TODO
+
+
+
+
+
+# ---------------------------------------------------------------------------- #
+# Run analyses for completer sample ----
+# ---------------------------------------------------------------------------- #
+
+# Contemporaneous models using POMP scores
+
+# TODO
+
+
+
+
+
+# Contemporaneous models using average item scores
+
+# TODO
+
+
+
+
+
+# Lagged models using POMP scores
+
+# TODO
+
+
+
+
+
+# Lagged models using average item scores
+
+# TODO
 
 
 
