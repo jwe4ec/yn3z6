417864e94049846bb8b4fc61cbcd18428815314b
Jeremy Eberle <jwe4ec@virginia.edu>
2021-05-09 23:51:52 -0400

    Compute between-person effects

--- syntax/5_compute_analysis_variables.R
+++ syntax/5_compute_analysis_variables.R
@@ -45,13 +45,47 @@ groundhog_day <- version_control()
 
 
 # ---------------------------------------------------------------------------- #
+# Define functions used throughout script ----
+# ---------------------------------------------------------------------------- #
+
+# Define function to compute between-person effects (i.e., person mean)
+
+compute_btw <- function(df, avg_item_colname, pomp_colname) {
+  avg_item_btw_colname <- paste0(avg_item_colname, "_btw")
+  pomp_btw_colname <- paste0(pomp_colname, "_btw")
+  
+  person_means <- aggregate(df[, c(avg_item_colname, pomp_colname)],
+                            list(df$ResearchID),
+                            mean, na.rm = TRUE)
+  names(person_means)[1] <- "ResearchID"
+  names(person_means)[2] <- avg_item_btw_colname
+  names(person_means)[3] <- pomp_btw_colname
+  
+  output <- merge(df, person_means, by = "ResearchID", all.x = TRUE)
+  output[is.na(output[, avg_item_colname]), 
+         c(avg_item_btw_colname, pomp_btw_colname)] <- NA
+  return(output)
+}
+
+# ---------------------------------------------------------------------------- #
 # Import data ----
 # ---------------------------------------------------------------------------- #
 
 load("./data/intermediate/data2.Rdata")
 
+data3 <- data2
+
 # ---------------------------------------------------------------------------- #
-# Compute Analysis Variables ----
+# Compute between-person effects (i.e., person mean) ----
+# ---------------------------------------------------------------------------- #
+
+data3$ders <- compute_btw(data2$ders, "drtotl_m_imp", "drtotl_m_imp_pomp")
+data3$dbt_wccl <- compute_btw(data2$dbt_wccl, "meanDSS", "meanDSS_pomp")
+data3$doss <- compute_btw(data2$doss, "cnDoSS", "cnDoSS_pomp")
+data3$kims <- compute_btw(data2$kims, "KMTOT", "KMTOT_pomp")
+
+# ---------------------------------------------------------------------------- #
+# Compute within-person effects (i.e., person-mean centered scores) ----
 # ---------------------------------------------------------------------------- #
 
 # TODO
